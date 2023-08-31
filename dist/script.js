756048d64533c21daed54c4d603fd308a8853f5c
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-08-31 22:07:59 +0300

    fix: bug with html-collection

--- dist/script.js
+++ dist/script.js
@@ -15,7 +15,7 @@ __webpack_require__.r(__webpack_exports__);
 class Slider {
   constructor(page, btns) {
     this.page = document.querySelector(page);
-    this.slides = this.page.children;
+    this.slides = Array.from(this.page.children);
     this.btns = document.querySelectorAll(btns);
     this.slideIndex = 1;
   }
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,7 +1,7 @@
 export default class Slider {
     constructor(page, btns){
         this.page = document.querySelector(page);
-        this.slides = this.page.children;
+        this.slides = Array.from(this.page.children);
         this.btns = document.querySelectorAll(btns);
         this.slideIndex = 1;
     }
