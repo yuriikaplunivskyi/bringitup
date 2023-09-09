4d33a112a136402a68865dc0d42fa35d4897ad28
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-09-09 12:42:06 +0300

    feat: mask and mail validation

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,12 +1,79 @@
 export default class Form {
     constructor(form) {
         this.forms = document.querySelectorAll(form);
+        this.inputs = document.querySelectorAll('input');
         this.message = {
             loading: 'Загрузка...',
             success: 'Спасибо! Скоро мы с вами свяжемся',
             failure: 'Что-то пошло не так...'
         };
         this.path = 'assets/question.php';
+    }
+
+    clearInputs() {
+        this.inputs.forEach(item => {
+            item.value = '';
+        })
+    }
+
+    checkMailInputs () {
+        const mailInputs = document.querySelectorAll('[type="email"]');
+    
+        mailInputs.forEach(input => {
+            input.addEventListener('keypress', function(e) {
+                if(e.key.match(/[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                }
+            })
+        })
+    }
+
+    initMask() {
+        let setCursorPosition = (pos , elem) => {
+            elem.focus();
+    
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+    
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+    
+        function createMask () {
+            let matrix = '+1 (___) ___-____',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, '');
+    
+            if (def.length >= val.length) {
+                val = def;
+            }
+    
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            })
+    
+            if (event.type === 'blur') {
+                if (this.value.length == 2) {
+                    this.value = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this)
+            }
+        }
+    
+        let inputs = document.querySelectorAll('[name="phone"]');
+    
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask)
+            input.addEventListener('blur', createMask)
+        })
     }
 
     async postData (url, data) {
@@ -19,6 +86,8 @@ export default class Form {
     }
 
     init () {
+        this.initMask();
+        this.checkMailInputs()
         this.forms.forEach(item => {
             item.addEventListener("submit", (e) => {
                 e.preventDefault();
@@ -42,6 +111,12 @@ export default class Form {
                     })
                     .catch(() => {
                         statusMessage.textContent = this.message.failure;
+                    })
+                    .finally(() => {
+                        this.clearInputs();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 6000)
                     })
             })
         })
